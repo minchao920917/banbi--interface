e33c2d392c3703235db3f6fde7279cf892efe2f5
minchao920917 <499790879@qq.com>
2018-07-24 19:01:53 +0800

    接口联调完成

--- js/community.js
+++ js/community.js
@@ -7,7 +7,7 @@ $(function(){
         //创建Vue实例
         var createVm =function(){
             var vm = new Vue({
-                el:'',
+                el:'.information',
                 data:{
                     items: []
                 },
@@ -17,6 +17,23 @@ $(function(){
                     //对一个包含对象的数组的排序，需要提供一个对象键并以此值来进行排序
                     order: function() {
                         getListData(this,req);
+                    }
+                },
+                filters:{
+                    formatPassTime(startTime) {
+                        var currentTime = Date.parse(new Date()),
+                            time = currentTime - Date.parse(new Date(startTime)),
+                            day = parseInt(time / (1000 * 60 * 60 * 24)),
+                            hour = parseInt(time / (1000 * 60 * 60)),
+                            min = parseInt(time / (1000 * 60)),
+                            month = parseInt(day / 30),
+                            year = parseInt(month / 12);
+                        if (year) return year + "年前"
+                        if (month) return month + "个月前"
+                        if (day) return day + "天前"
+                        if (hour) return hour + "小时前"
+                        if (min) return min + "分钟前"
+                        else return '刚刚'
                     }
                 }
 
@@ -34,9 +51,10 @@ $(function(){
                 success: function(data){
 
                     var recommendVM = new Vue({
-                        el:'.rec-list',
+                        el:'.recommend',
                         data:{
-                            recommends: data.data.recommend
+                            recommends: data.data.recommend,
+                            picked:data.data.picked[0]
                         },
                         computed: {
                             topics:function(){
@@ -56,31 +74,23 @@ $(function(){
                             }
                         },
                         methods:{
-                            //对一个包含对象的数组的排序，需要提供一个对象键并以此值来进行排序
-                            order: function() {
-                                getListData(this,req);
-                            }
+                            
                         }
 
                     });
-
-                    console.log(data.data.recommend);
-
                 }
             });
 
         }
 
-        //初始化表格
+        //初始化列表
         var getListData = function(vm,req){
 
             $.ajax({
                 type: "GET",
-                url: "http://api.banbijie.com/?s=Currency.search",
+                url: "http://api.banbijie.com/?s=Notices.GetList",
                 data: req,
                 success: function(data){
-                    // console.log(req);
-                    // console.log(data.data);
                     vm.items = data.data.items;
                 }
             });
@@ -92,7 +102,7 @@ $(function(){
         var getMoreTableData = function(vm,req){
             $.ajax({
                 type: "GET",
-                url: "",
+                url: "http://api.banbijie.com/?s=Notices.GetList",
                 data: req,
                 success: function(data){
 
--- js/details.js
+++ js/details.js
@@ -13,17 +13,65 @@ $(function(){
             r = null;
             return context == null || context == "" || context == "undefined" ? "" : context;
         }
+
+         //创建Vue实例
+        var createVm =function(){
+            var vm = new Vue({
+                el:'.information',
+                data:{
+                    article: []
+                },
+                computed: {
+                },
+                methods:{
+                   
+                },
+                filters:{
+                    formatPassTime(startTime) {
+                        var currentTime = Date.parse(new Date()),
+                            time = currentTime - Date.parse(new Date(startTime)),
+                            day = parseInt(time / (1000 * 60 * 60 * 24)),
+                            hour = parseInt(time / (1000 * 60 * 60)),
+                            min = parseInt(time / (1000 * 60)),
+                            month = parseInt(day / 30),
+                            year = parseInt(month / 12);
+                        if (year) return year + "年前"
+                        if (month) return month + "个月前"
+                        if (day) return day + "天前"
+                        if (hour) return hour + "小时前"
+                        if (min) return min + "分钟前"
+                        else return '刚刚'
+                    }
+                }
+
+            })
+            return vm;
+        }
+
         /**
          *  根据id获取文章信息
          * */
-        function  initArticle() {
+        function  initArticle(vm) {
             var id=GetQueryString("id");
             console.log(id);
+            $.ajax({
+                type: "GET",
+                url: "http://api.banbijie.com/?s=Notices.GetNotice",
+                data: {
+                    id:id
+                },
+                success: function(data){
+
+                    vm.article = data.data.items[0];
+                }
+            });
+
         }
 
         //初始化函数
         var init = function(){
-            initArticle();//初始化文章
+            var vm = createVm();//创建列表
+            initArticle(vm);//初始化文章
 
         };
 
